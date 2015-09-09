4ec8eb2df6595447d3056492254369a6cd911949
andela.bmosigisi <brianmosigisi@yahoo.com>
2015-09-09 11:49:25 +0300

    Connection Settings modified

--- projects/conn.php
+++ projects/conn.php
@@ -2,8 +2,8 @@
 
 $host = "localhost";
 $dbname = "learn-php";
-$user = "prof";
-$pass = "pr0f";
+$user = "andela";
+$pass = "andela";
 
 try {
 	$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
